64886ca9be481e724105f0c3a21d70f5e86de62c
Bakhtiyar Tokpayev <btokpayev@gmail.com>
2024-10-18 00:29:58 +0500

    Lesson 1 homework

---
+++ FamilyMembers.cs
@@ -0,0 +1,145 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RelationshipApp1
+{
+	public enum Gender { male, female }
+	public class FamilyMember
+	{
+		private bool screenMaximized = false;
+		public int Age { get; set; }
+		public Gender Gender { get; set; }
+		public string FullName { get; set; }
+		public FamilyMember Mother { get; set; }
+		public FamilyMember Father { get; set; }
+    public FamilyMember MyHalf { get; set; }
+		public FamilyMember[] Kids = null;
+
+
+		public void AddMyHalf(FamilyMember m)
+		{
+			if (MyHalf == null)
+			{
+				if (Gender != m.Gender)
+				{
+					MyHalf = m;
+					m.MyHalf = this;
+					if (Kids != null && Kids.Length > 0)
+						m.Kids = Kids;
+				}
+			}
+		}
+		public void AddKids(params FamilyMember[] kids)
+		{
+			Kids = kids;
+			if (MyHalf!=null)
+				MyHalf.Kids = Kids;
+		}
+    public FamilyMember[] GetGrandFathersName()
+		{
+			return new FamilyMember[] { Father?.Father, Mother?.Father };
+		}
+		public FamilyMember[] GetGrandMothersName()
+		{
+			return new FamilyMember[] { Father?.Mother, Mother?.Mother };
+		}
+		public FamilyMember GetFather()
+		{
+			return Father;
+		}
+		public FamilyMember GetMother()
+		{
+			return Mother;
+		}
+		public FamilyMember GetMyHalf()
+		{
+			return MyHalf;
+		}
+		public FamilyMember[] GetKids()
+		{
+			return Kids;
+		}
+		public string GetCountString(int count)
+		{
+			if (count > 0)
+			{
+				switch (count)
+				{
+					case 1: return "First"; break;
+					case 2: return "Second";break;
+					case 3: return "Third"; break;
+					case 4: return "Forth"; break;
+					case 5: return "Fifth"; break;
+					case 6: return "Sixth"; break;
+					case 7: return "Seventh"; break;
+					case 8: return "Eighth"; break;
+					case 9: return "Ninth"; break;
+					case 10: return "Tenth"; break;
+					default: return "K-th"; break;
+				}
+			}
+			else
+				return "";
+		}
+		public void ShowMyCloseRelatives1()
+		{
+			string auxStr = String.Empty;
+			int countSons = 1, countDaughters = 1, width, height;
+
+			if (!screenMaximized)
+			{
+				width = Console.LargestWindowWidth;
+				height = Console.LargestWindowHeight;
+				Console.SetWindowSize(width, height);
+			}
+
+			if (Father != null)
+				Console.WriteLine($"My father: {Father.FullName}");
+			if (Mother != null)
+				Console.WriteLine($"My mother: {Mother.FullName}\n");
+			if (MyHalf != null)
+			{
+				if (MyHalf.Gender == Gender.male)
+					auxStr = "husband";
+				else
+					auxStr = "wife";
+				//if (MyHalf.Father != null)
+				//	Console.WriteLine($"My {auxStr}'s Father: {MyHalf.Father.FullName}");
+				//if (MyHalf.Mother != null)
+				//	Console.WriteLine($"My {auxStr}'s Mother: {MyHalf.Mother.FullName}");
+				Console.WriteLine($"My {auxStr}: {MyHalf.FullName}");
+			}
+			Console.WriteLine($"Me: {FullName}\n");
+			if (Kids != null && Kids.Length > 0)
+			{
+				Console.WriteLine("Kids:");
+				foreach (var k in Kids)
+				{
+					if (k.Gender == Gender.male)
+						Console.WriteLine($"{GetCountString(countSons++)} son: {k.FullName}");
+					else
+						Console.WriteLine($"{GetCountString(countDaughters++)} daughter: {k.FullName}");
+				}
+				Console.WriteLine("\n");
+			}
+		}
+		public void ShowMyCloseRelatives2()
+		{
+			int c, cl, cr, cll, clr, crl, crr, width, height;
+			string mF, mM, hF, hM, m, h;
+
+			if (!screenMaximized)
+			{
+				width = Console.LargestWindowWidth;
+				height = Console.LargestWindowHeight;
+				Console.SetWindowSize(width,height);
+			}
+				
+
+
+		}
+	}
+}
---
+++ Program.cs
@@ -0,0 +1,112 @@
+﻿using RelationshipApp1;
+using System.Net.Cache;
+
+internal class Program
+{
+	static void Main(string[] args)
+	{
+		FamilyMember GrandFatherFirst = new FamilyMember()
+		{
+			FullName = "Karatayev Kanat Myrzakhanovich",
+			Age = 60,
+			Gender = Gender.male
+		};
+
+		FamilyMember GrandFatherSecond = new FamilyMember()
+		{
+			FullName = "Aytbayev Mugradim Kamalovich",
+			Age = 64,
+			Gender = Gender.male
+		};
+
+		FamilyMember GrandMotherFirst = new FamilyMember()
+		{
+			FullName = "Akhtanova Aliya Ibrayevna",
+			Age = 57,
+			Gender = Gender.female
+		};
+
+		FamilyMember GrandMotherSecond = new FamilyMember()
+		{
+			FullName = "Zhumagulova Adel Iskakovna",
+			Age = 58,
+			Gender = Gender.female
+		};
+
+		FamilyMember Father = new FamilyMember()
+		{
+			FullName = "Myrzakhanov Altay Kanatovich",
+			Age = 35,
+			Gender = Gender.male,
+			Father= GrandFatherFirst,
+			Mother= GrandMotherFirst
+		};
+
+		FamilyMember Mother = new FamilyMember()
+		{
+			FullName = "Kamalova Aysha Mugradimovna",
+			Age = 33,
+			Gender = Gender.female,
+			Father=GrandFatherSecond,
+			Mother= GrandMotherSecond
+		};
+
+		FamilyMember Son1 = new FamilyMember()
+		{
+			FullName = "Kanatov Zhengiskhan Altayevich",
+			Age = 10,
+			Gender = Gender.male,
+			Father = Father,
+			Mother = Mother
+		};
+
+		FamilyMember Daughter = new FamilyMember()
+		{
+			FullName = "Kanatova Aizhan Altayevna",
+			Age = 8,
+			Gender = Gender.female,
+			Father = Father,
+			Mother = Mother
+		};
+
+		FamilyMember Son2 = new FamilyMember()
+		{
+			FullName = "Kanatov Baiyrkhan Altayevich",
+			Age = 6,
+			Gender = Gender.male,
+			Father = Father,
+			Mother = Mother
+		};
+
+		GrandFatherFirst.AddMyHalf(GrandMotherFirst);
+		GrandFatherFirst.AddKids(Father);
+
+		GrandFatherSecond.AddMyHalf(GrandMotherSecond);
+		GrandFatherSecond.AddKids(Mother);
+
+		Father.AddKids(Son1, Daughter, Son2);
+		Father.AddMyHalf(Mother);
+
+		Father.ShowMyCloseRelatives1();
+		Father.ShowMyCloseRelatives2();
+
+		//Mother.AddKids(Son1, Daughter, Son2);
+		//Mother.AddMyHalf(Father);
+
+		//Mother.ShowMyCloseRelatives1();
+		//Mother.ShowMyCloseRelatives2();
+
+		/*
+		var GrandFathers = Son.GetGrandFathersName();
+		var GrandMothers = Son.GetGrandMothersName();
+
+		Console.WriteLine(GrandFathers[0]?.FullName);
+		Console.WriteLine(GrandMothers[0]?.FullName);
+		Console.WriteLine(GrandFathers[1]?.FullName);
+		Console.WriteLine(GrandMothers[1]?.FullName);
+		*/
+	}
+}
+
+
+
